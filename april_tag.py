4acbb6350ebd84866c7df2215f9d10b65ec6abab
Shan <shh003@eng.ucsd.edu>
2019-07-06 01:13:08 +0000

    Add files via upload
    print location and orientation of tags

--- april_tag.py
+++ april_tag.py
@@ -4,34 +4,44 @@ import numpy as np
 import pickle
 import picamera
 import time
+from math import atan2,degrees
+
+def draw_tag(frame,center,corners):
+	cv2.circle(frame,tuple(center.astype(int)),5,(0,0,255),3)
+	cv2.circle(frame,tuple(corners[0].astype(int)),5,(0,0,255),3)
+	cv2.circle(frame,tuple(corners[1].astype(int)),5,(0,255,0),3)
+	cv2.circle(frame,tuple(corners[2].astype(int)),5,(255,0,0),3)
+	cv2.circle(frame,tuple(corners[3].astype(int)),5,(0,0,0),3)
+	mid = [np.int((corners[0][0]+corners[1][0])/2),np.int((corners[0][1]+corners[1][1])/2)]
+	cv2.line(frame,tuple(center.astype(int)),tuple(mid),(0,0,255),5)
+	angle = degrees(atan2(corners[1][1]-corners[0][1],corners[1][0]-corners[0][0]))
+	#cv2.putText(frame,"angle: "+str(np.int(angle)),tuple(center.astype(int)),cv2.FONT_HERSHEY_SIMPLEX,2,128)
+	return frame, angle
+	
+def main():
+	video_capture = cv2.VideoCapture(0)
+	video_capture.set(3, 640)
+	video_capture.set(4, 480)
+	
+	detector = apriltag.Detector(searchpath=apriltag._get_demo_searchpath())
+	while (True):
+	    try:
+	        _, frame = video_capture.read()
+		gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	        result = detector.detect(gray_frame)
+	        
+	        if result:
+	            center = result[0].center
+		    corners = result[0].corners
+		    frame, angle = draw_tag(frame,center,corners)
+		    print(angle)
+
+	        cv2.imshow("apriltags",frame)
+	        cv2.waitKey(1)
+	    except KeyboardInterrupt:
+	        break
+	video_capture.release
 
 
-video_capture = cv2.VideoCapture(0)
-video_capture.set(3, 640)
-video_capture.set(4, 480)
-
-detector = apriltag.Detector()
-while (True):
-    try:
-        _, frame = video_capture.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        result = detector.detect(frame)
-        
-        if result:
-            print(result)
-            tf = result[0].tag_family
-            cx =  result[0].center[0]
-            cy =  result[0].center[1]
-            #xyDict = {"CX": cx,"CY": cy}
-            #xyArray = np.array([cx,cy])
-            #np.save('/home/pi/TritonTown/MQTT/xyArray.npy', xyArray)
-            #with open('/home/pi/TritonTown/MQTT/xyDict.pickle', 'wb') as handle:
-               #pickle.dump(xyDict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-            #print("ourxyArray = ", xyArray)
-            #print(tf)
-            #print(cx,cy)
-        cv2.imshow("apriltags",frame)
-        cv2.waitKey(1)
-    except KeyboardInterrupt:
-        break
-video_capture.release
+if __name__=="__main__":
+	main()
